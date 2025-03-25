971dc8ed5962ee58e2d942c7a1bd8ed5b53c7d1e
imad890 <imadser890@gmail.com>
2025-03-25 11:21:24 +0100

    Primer commit con los módulos de Odoo 18

---
+++ Abogados/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Abogados',
+    'version':'2.0',
+    'author':"Prueba",
+    'category': 'AbogadosAsociados',
+    "description": "Permite la gestion de Abogados y Procuradores.",
+    
+
+    'application': True,
+    'installable': True,
+    'data': [
+        'views/viewsabogados.xml',
+        'views/viewsprocuradores.xml',
+        'security.xml'
+    ],
+    'icon': '/Abogados/static/description/icon.png'
+}
