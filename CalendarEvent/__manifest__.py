0f888d9d9630ae8ce7541f1150f60259c65ee938
imad890 <imadser890@gmail.com>
2025-04-15 13:41:59 +0200

    subiendo todos los módulos al repositorio con trámites corregido

---
+++ CalendarEvent/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Extensión Calendar Event para Trámites',
+    'version': '1.0',
+    'author': 'prueba',
+    'category': 'Calendario',
+    'description': 'Añade funcionalidades relacionadas con trámites al calendario.',
+    'summary': 'Añade funcionalidades relacionadas con trámites al calendario.',
+    'sequence': 10,
+    'depends': ['base', 'calendar'],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
